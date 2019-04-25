647002964e4cec8c8c25ffcd52a1022373ca0aab
YiJiangFengYun <yijiangfengyun@outlook.com>
2019-04-25 18:30:55 +0800

    Add feature to ensure the directory of output existing.

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 // require modules
+var fsExtra = require("fs-extra");
+var path = require("path");
 var init_1 = require("./init");
 var archiver = require("./archiver");
 function archive(info) {
     return init_1.init()
+        .then(function () {
+        //Ensure the directory of output existing
+        return fsExtra.ensureDir(path.dirname(info.output));
+    })
         .then(function () {
         return archiver.archive(info);
     });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 // require modules
+import * as fsExtra from "fs-extra";
+import * as path from "path";
 import { init } from "./init";
-
 import * as archiver from "./archiver";
 
 export function archive(info: archiver.ArchiveInfo) {
     return init()
+    .then(() => {
+        //Ensure the directory of output existing
+        return fsExtra.ensureDir(path.dirname(info.output));
+    })
     .then(() => {
         return archiver.archive(info);
     });
